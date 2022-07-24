01aeaa530543be587a056757cc039444816b0e7e
Olga Velichko <Ovelichko@Paylocity.com>
2022-07-23 20:48:53 -0500

    added foreach loop

--- js/ratings.js
+++ js/ratings.js
@@ -1,4 +1,5 @@
 const collect_ratings = ()=>{
+
    const ratings={
     count :0,
     sum : 0,
@@ -8,4 +9,21 @@ const collect_ratings = ()=>{
     // input element from HTML
     const elements= document.querySelectorAll('.rating');
 
+    //cycling thought elements
+    elements.forEach(element=>{
+        rating =parseInt(element.id.replace('star',''));
+        ratings.count += parseInt(element.value);
+        rating.sum +=parseInt(element.value)*rating;
+    })
+
+    if(rating.count!==0){
+        ratings.average = rating.sum/rating.count;
+    }
+
+    return ratings;
+
 }
+document.addEventListener('change',()=>{
+   const rating= collect_ratings();
+   document.querySelector('#average').value = rating.average.toFixed(2);
+})
