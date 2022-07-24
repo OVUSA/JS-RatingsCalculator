4250de867d15649efcf2db6a292a7927405bd920
Olga Velichko <Ovelichko@Paylocity.com>
2022-07-23 20:41:44 -0500

    added rating function

---
+++ js/ratings.js
@@ -0,0 +1,11 @@
+const collect_ratings = ()=>{
+   const ratings={
+    count :0,
+    sum : 0,
+    average :0
+    }
+    let rating = 0;
+    // input element from HTML
+    const elements= document.querySelectorAll('.rating');
+
+}
